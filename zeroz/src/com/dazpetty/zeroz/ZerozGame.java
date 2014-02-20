3970f647e5feaaf2f84be799a2c8875bced53350
daz <neuroc453@gmail.com>
2014-02-20 17:29:43 +1100

    added test animation

--- zeroz/src/com/dazpetty/zeroz/ZerozGame.java
+++ zeroz/src/com/dazpetty/zeroz/ZerozGame.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -32,8 +33,18 @@ public class ZerozGame implements ApplicationListener {
 	private TiledMap map;
 	private TiledMapRenderer renderer;
 	public TiledMapTileLayer collisionLayer;
-	
 	private Vector2 playerstart = new Vector2(4,4);
+	
+	
+	private static final int        FRAME_COLS = 4;         // #1
+	private static final int        FRAME_ROWS = 6;         // #2
+	  
+	Animation                       walkAnimation;          // #3
+	Texture                         walkSheet;              // #4
+	TextureRegion[]                 walkFrames;             // #5
+	SpriteBatch                     spriteBatch;            // #6
+	TextureRegion                   currentFrame;           // #7
+	float stateTime;
 	
 	//Array<Vector3> waypoints;
 	//Vector<Vector3f>
@@ -43,6 +54,24 @@ public class ZerozGame implements ApplicationListener {
 	
 	@Override
 	public void create() {	
+		
+		
+		walkSheet = new Texture(Gdx.files.internal("data/gfx/punk/run.png"));     // #9
+		   TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / 
+				   FRAME_COLS, walkSheet.getHeight() / FRAME_ROWS);                                // #10
+       walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+       int index = 0;
+       for (int i = 0; i < FRAME_ROWS; i++) {
+               for (int j = 0; j < FRAME_COLS; j++) {
+                       walkFrames[index++] = tmp[i][j];
+               }
+       }
+       walkAnimation = new Animation(0.025f, walkFrames);              // #11
+       spriteBatch = new SpriteBatch();                                // #12
+       stateTime = 0f;                                                 // #13
+		
+       
+       
 		map = new TmxMapLoader().load("data/testmap.tmx");	
 		
 		collisionLayer = (TiledMapTileLayer) map.getLayers().get("Layer 0");
@@ -103,6 +132,13 @@ public class ZerozGame implements ApplicationListener {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		
+		stateTime += Gdx.graphics.getDeltaTime();                       // #15
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);      // #16
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, 50, 50);                         // #17
+        spriteBatch.end();
+        
+        
 		batch.setProjectionMatrix(camera.combined);
 
 		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
